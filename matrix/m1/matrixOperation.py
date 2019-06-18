0ac072b4bfcfa900da0b095de80ba28eb4836834
3ntropia <fmartinhuber@gmail.com>
2019-06-18 01:23:29 -0300

    Matrix!

---
+++ matrix/m1/matrixOperation.py
@@ -0,0 +1,127 @@
+"""Desarrollar cada una de las siguientes funciones y escribir un programa que permita
+verificar su funcionamiento, imprimiendo la matriz luego de invocar a cada función:
+a. Cargar números enteros en una matriz de N x N, ingresando los datos desde
+teclado.
+b. Ordenar en forma ascendente cada una de las filas de la matriz.
+c. Intercambiar dos filas, cuyos números se reciben como parámetro.
+d. Intercambiar dos columnas dadas, cuyos números se reciben como parámetro.
+e. Intercambiar una fila por una columna, cuyos números se reciben como parámetro.
+f. Transponer la matriz sobre si misma. (intercambiar cada elemento Aij por Aji)
+g. Calcular el promedio de los elementos de una fila, cuyo número se recibe como
+parámetro.
+h. Calcular el porcentaje de elementos con valor impar en una columna, cuyo número
+se recibe como parámetro.
+i. Determinar si la matriz es simétrica con respecto a su diagonal principal.
+j. Determinar si la matriz es simétrica con respecto a su diagonal secundaria.
+k. Determinar qué columnas de la matriz son palíndromos (capicúas), devolviendo
+una lista con los números de las mismas.
+NOTA: El valor de N debe establecerlo el programador o ingresarse por teclado,
+pero las funciones deben servir aunque este valor se modifique."""
+
+
+def load_numbers(size):
+    m = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(int(input("ingrese num para i=" + str(i) + " j=" + str(j) + " :")))
+        m.append(row)
+    return m
+
+
+def get_column(m, c):
+    return [row[c] for row in m]
+
+
+def replace_column(m, c, column):
+    for i in range(len(column)):
+        row = m[i]
+        del row[c]
+        row.insert(c, column[i])
+    return m
+
+
+def sort_row(m, r):
+    return sorted(m[r])
+
+
+def sort_whole_matrix_row(m):
+    m_sort = []
+    for j in range(len(m)):
+        m_sort.append(sort_row(m, j))
+    return m_sort
+
+
+def order_column(m, c):
+    column = get_column(m, c)
+    column.sort()
+    return replace_column(m, c, column)
+
+
+def change_row(m, r1, r2):
+    row1 = m[r1].copy()
+    row2 = m[r2].copy()
+    del m[r1]
+    m.insert(r1, row2)
+    del m[r2]
+    m.insert(r2, row1)
+    return m
+
+
+def change_column(m, c1, c2):
+    column1 = get_column(m, c1)
+    column2 = get_column(m, c2)
+    replace_column(m, c1, column2)
+    replace_column(m, c2, column1)
+    return m
+
+
+def change_row_to_column(m, r, c):
+    row = m[r]
+    replace_column(m, c, row.copy())
+    return m
+
+
+def transpose_matrix(m):
+    m_transpose = []
+    for i in range(len(m)):
+        m_transpose.append(get_column(m, i))
+    return m_transpose
+
+
+def is_regular(m):
+    for j in m:
+        if len(j) != len(m):
+            return False
+    return True
+
+
+def is_simetric(m):
+    m_copy = m.copy()
+    if is_regular(m):
+        return m_copy == transpose_matrix(m)
+    return False
+
+
+def inversed_transpose_matrix(m):
+    m_transpose = []
+    for i in range(len(m)):
+        m_transpose.append(reversed(get_column(m, i)))
+    return m_transpose
+
+
+def row_prom(m, r):
+    int_sum = 0
+    for j in m[r]:
+        int_sum += j
+    return int_sum / len(m[r])
+
+#TODO
+# Calcular el porcentaje de elementos con valor impar en una columna, cuyo número se recibe como parámetro.
+# Determinar si la matriz es simétrica con respecto a su diagonal secundaria.
+# Determinar qué columnas de la matriz son palíndromos (capicúas), devolviendo una lista con los números de las mismas.
+
+
+if __name__ == "__main__":
+    ma = load_numbers(3)
+    print(ma)
---
+++ matrix/m1/testMatrix.py
@@ -0,0 +1,60 @@
+import unittest
+import matrix.m1.matrixOperation as o
+
+
+class MyTestCase(unittest.TestCase):
+    __m__ = [[1, 2, 3], [9, 8, 11], [33, 2, 5]]
+    __m_sorted__ = [[1, 2, 3], [8, 9, 11], [2, 5, 33]]
+    __m_i__ = [[1, 2, 3], [9, 8, 11], [33, 2, 5], [12, 55, 3], [12, 73, 99]]
+
+    def test_get_column(self):
+        self.assertEqual(o.get_column(MyTestCase.__m__, 1), [2, 8, 2])
+
+    def test_get_column_irregular_matrix(self):
+        self.assertEqual(o.get_column(MyTestCase.__m_i__, 1), [2, 8, 2, 55, 73])
+
+    def test_replace_column(self):
+        mm = o.replace_column(MyTestCase.__m__, 1, [2, 3, 4])
+        self.assertEqual(o.get_column(mm, 1), [2, 3, 4])
+
+    def test_order_column(self):
+        self.assertEqual(o.get_column(o.order_column(MyTestCase.__m__, 1), 1), [2, 2, 8])
+
+    def test_order_row(self):
+        self.assertEqual(o.sort_row(MyTestCase.__m__, 1), [8, 9, 11])
+
+    def test_order_matrix_rows(self):
+        self.assertEqual(o.sort_whole_matrix_row(MyTestCase.__m__), MyTestCase.__m_sorted__)
+
+    def test_change_rows(self):
+        self.assertEqual(o.change_row(MyTestCase.__m__, 0, 1), [[9, 8, 11], [1, 2, 3], [33, 2, 5]])
+
+    def test_change_columns(self):
+        self.assertEqual(o.change_column(MyTestCase.__m__, 0, 1), [[2, 1, 3], [8, 9, 11], [2, 33, 5]])
+
+    def test_row_to_column(self):
+        self.assertEqual(o.change_row_to_column(MyTestCase.__m__, 0, 0), [[1, 2, 3], [2, 8, 11], [3, 2, 5]])
+
+    def test_transpose(self):
+        self.assertEqual(o.transpose_matrix(MyTestCase.__m__), [[1, 9, 33], [2, 8, 2], [3, 11, 5]])
+
+    def test_is_regular(self):
+        self.assertEqual(o.is_regular(MyTestCase.__m__), True)
+
+    def test_is_iregular(self):
+        self.assertEqual(o.is_regular(MyTestCase.__m_i__), False)
+
+    def test_is_not_simetric_regular(self):
+        self.assertEqual(o.is_simetric(MyTestCase.__m__), False)
+
+    def test_is_not_simetric_not_regular(self):
+        self.assertEqual(o.is_simetric(MyTestCase.__m_i__), False)
+
+    def test_is_simetric(self):
+        self.assertEqual(o.is_simetric([[1, 2], [2, 3]]), True)
+
+    def test_inversed_transposed(self):
+        self.assertEqual(o.inversed_transpose_matrix([[1, 2], [3, 1]]), [[1, 2], [3, 1]])
+
+if __name__ == '__main__':
+    unittest.main()
