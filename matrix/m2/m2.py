5ab5e5cdcec0a93143944d64850e66ccaa315d77
3ntropia <fmartinhuber@gmail.com>
2019-06-19 01:02:28 -0300

    Matrix 2!

--- matrix/m2/m2.py
+++ matrix/m2/m2.py
@@ -18,3 +18,63 @@ g:  1 2 3 4       h: 1 2 4 7             i: 1 2 6 7
     11 16 15         6 6 9 12            14 4 9 12 14
     10 9 8 7         10 13 15            16 10 11 15 16"""
 
+
+def first_matrix(size):
+    m = []
+    num = 1
+    for i in range(size):
+        m.append([num if y == i else 0 for y in range(size)])
+        num += 2
+    return m
+
+
+def second_matrix(size):
+    m = []
+    num = 1
+    for i in range(size):
+        m.insert(0, [num if y == i else 0 for y in range(size)])
+        num *= 3
+    return m
+
+
+def third_matrix(size):
+    m = []
+    num = size
+    for i in range(size):
+        m.append([num if y <= i else 0 for y in range(size)])
+        num -= 1
+    return m
+
+
+def fourth_matrix(size):
+    m = []
+    num = size * 2
+    for i in range(size):
+        m.append([int(num) for _ in range(size)])
+        num /= 2
+    return m
+
+
+def fifth_matrix(size):
+    m = []
+    num = 1
+    for i in range(size):
+        if i % 2 == 0:
+            m.append([num if y % 2 != 0 else 0 for y in range(size)])
+        else:
+            m.append([num if y % 2 == 0 else 0 for y in range(size)])
+        num += 1
+    return m
+
+
+if __name__ == "__main__":
+    m1 = first_matrix(4)
+    print(m1)
+    m2 = second_matrix(4)
+    print(m2)
+    m3 = third_matrix(4)
+    print(m3)
+    m4 = fourth_matrix(4)
+    print(m4)
+    m5 = fifth_matrix(4)
+    print(m5)
